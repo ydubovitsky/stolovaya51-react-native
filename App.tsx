a985336d141f6837a5fe7fad3f6b411add58f824
y-dubovitsky <pXmJ5M9dvP>
2023-01-28 20:35:05 +0300

    Fixed all problems with crashes on devices

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
+import "react-native-gesture-handler";
+import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet } from "react-native";
-import "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MainStackNavigator from "./src/app/routes/main-stack-navigator.route";
 
 const App = (): JSX.Element => {
   return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer>
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
         <MainStackNavigator />
-      </NavigationContainer>
-      {/* <StartScreen /> */}
-    </SafeAreaView>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
