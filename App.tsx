91312018d86fb3f896f963dac12e86d4af103704
y-dubovitsky <pXmJ5M9dvP>
2023-01-30 21:20:26 +0300

    The account screen redesigned, react-native-paper library added

--- App.tsx
+++ App.tsx
@@ -3,14 +3,17 @@ import * as React from "react";
 import "react-native-gesture-handler";
 import { Provider } from "react-redux";
 import MainStackNavigator from "./src/app/routes/main-stack-navigator.route";
+import { Provider as PaperProvider } from "react-native-paper";
 import { store } from "./src/redux/store";
 
 const App = (): JSX.Element => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <MainStackNavigator />
-      </NavigationContainer>
+      <PaperProvider>
+        <NavigationContainer>
+          <MainStackNavigator />
+        </NavigationContainer>
+      </PaperProvider>
     </Provider>
   );
 };
--- src/app/screens/account/account.screen.tsx
+++ src/app/screens/account/account.screen.tsx
@@ -1,30 +1,34 @@
 import { StackScreenProps } from "@react-navigation/stack/lib/typescript/src/types";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View
-} from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import * as Animatable from "react-native-animatable";
 import { BottomTabNavigatorParamList } from "../../routes/main-stack-navigator.route";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { Avatar } from "react-native-paper";
+const imageSource = require("./images/joanna-nix-walkup-Kp1Oz5fSxiw-unsplash.jpg");
 
 /**
  * StartScreen functional component!
- * @param param0 
- * @returns 
+ * @param param0
+ * @returns
  */
 const AccountScreen = ({
   route,
   navigation,
 }: StackScreenProps<BottomTabNavigatorParamList, "Аккаунт">): JSX.Element => {
-
   return (
     <SafeAreaView style={styles.container}>
       <Animatable.View animation="zoomInUp">
-        <Text>Account Screnn</Text>
+        <Avatar.Image size={300} source={imageSource} />
       </Animatable.View>
+      <View style={styles.description}>
+        <Text style={styles.text}>Здесь скоро будет ваш личный кабинет</Text>
+      </View>
+      <View style={styles.description}>
+        <Text style={styles.text}>
+          А пока вы можете ознакомиться с меню на сегодняшний день 😘
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
@@ -32,6 +36,15 @@ const AccountScreen = ({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  description: {
+    padding: "10%",
+  },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
   },
 });
 
