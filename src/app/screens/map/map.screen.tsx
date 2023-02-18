2223086144eca0707210dfece97bfba781d4edae
y-dubovitsky <pXmJ5M9dvP>
2023-02-18 17:52:30 +0300

    MapScreen is using WebView now

--- src/app/screens/map/map.screen.tsx
+++ src/app/screens/map/map.screen.tsx
@@ -4,6 +4,8 @@ import { Image, StyleSheet, View, Text } from "react-native";
 import { BottomTabNavigatorParamList } from "../../routes/main-stack-navigator.route";
 import { WebView } from "react-native-webview";
 
+const imageSource = require("./images/map-screen.png");
+
 /**
  * MapScreen functional component!
  * @param param0
@@ -18,10 +20,16 @@ const MapScreen = ({
 >): JSX.Element => {
   return (
     <View style={styles.container}>
-      <WebView
-        style={styles.mapContainer}
-        source={{ uri: "https://yandex.ru/maps/213/moscow/?ll=37.625930%2C55.861486&mode=poi&poi%5Bpoint%5D=37.625437%2C55.861986&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D97079608227&z=17.82" }}
-      />
+      <View style={styles.imageContainer}>
+        <Image source={imageSource} style={styles.image} />
+      </View>
+      <View style={styles.mapContainer}>
+        <WebView
+          source={{
+            uri: "https://yandex.ru/maps/213/moscow/?ll=37.625930%2C55.861486&mode=poi&poi%5Bpoint%5D=37.625437%2C55.861986&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D97079608227&z=17.82",
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -30,9 +38,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  imageContainer: {
+    flex: 2,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: "stretch",
+  },
   mapContainer: {
-    width: "100%",
-    height: "100%",
+    padding: 10,
+    flex: 4,
   },
 });
 
